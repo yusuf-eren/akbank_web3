969a5dd0a6c60b3f5a23f5141ce968f9be4cf555
yusuf-eren <erenyusuf170@gmail.com>
2022-09-28 19:00:23 +0300

    crowd fund

---
+++ Challenges/3_CrowdFund.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./IERC20.sol";
+
+contract CrowdFund {
+    event Launch(
+        uint256 id,
+        address indexed creator,
+        uint256 goal,
+        uint32 startAt,
+        uint32 endAt
+    );
+
+    event Cancel(uint256 id);
+
+    event Pledge(uint256 indexed id, address indexed caller, uint256 amount);
+
+    event Unpledge(uint256 indexed id, address indexed caller, uint256 amount);
+
+    event Refund(uint256 indexed id, address indexed caller, uint256 amount);
+
+    struct Campaign {
+        address creator;
+        uint256 goal;
+        uint256 pledged;
+        uint32 startAt;
+        uint32 endAt;
+        bool claimed;
+    }
+    // we use IERC20 token
+    // we use 1 token for security reasons
+    IERC20 public immutable token;
+    // everytime we create a new campaign
+    // this count will increment
+    uint256 public count;
+    // uint = id of the campaign
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(uint256 => mapping(address => uint256)) public pledgedAmount;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function launch(
+        uint256 _goal,
+        uint32 _startAt,
+        uint32 _endAt
+    ) external {
+        // startAt should be greater than or equal to current block timestamp
+        //  endAt should be greater than or equal to startAt
+        // it must be less than or equal to 90 days from the current block that timestamp
+        require(_startAt >= block.timestamp, "start at < now");
+        require(_endAt >= _startAt, "end at < start at");
+        require(_endAt <= block.timestamp + 90 days, "end at > max duration");
+
+        count += 1;
+        campaigns[count] = Campaign({
+            creator: msg.sender,
+            goal: _goal,
+            pledged: 0,
+            startAt: _startAt,
+            endAt: _endAt,
+            claimed: false
+        });
+
+        emit Launch(count, msg.sender, _goal, _startAt, _endAt);
+    }
+
+    function cancel(uint256 _id) external {
+        Campaign memory campaign = campaigns[_id];
+        require(msg.sender == campaign.creator, "not creator");
+        require(block.timestamp < campaign.startAt, "started");
+        delete campaigns[_id];
+        emit Cancel(_id);
+    }
+
+    function pledge(uint256 _id, uint256 _amount) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp >= campaign.startAt, "not started");
+        require(block.timestamp <= campaign.endAt, "ended");
+
+        campaign.pledged += _amount;
+        pledgedAmount[_id][msg.sender] += _amount;
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        emit Pledge(_id, msg.sender, _amount);
+    }
+
+    function unpledge(uint256 _id, uint256 _amount) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp <= campaign.endAt, "ended");
+        campaign.pledged -= _amount;
+        pledgedAmount[_id][msg.sender] -= _amount;
+        token.transfer(msg.sender, _amount);
+
+        emit Unpledge(_id, msg.sender, _amount);
+    }
+
+    function claim(uint256 _id) external {
+        Campaign storage campaign = campaigns[_id];
+        require(msg.sender == campaign.creator, "not creator");
+        require(block.timestamp > campaign.endAt, "not ended");
+        require(campaign.pledged >= campaign.goal, "pledged < goal");
+        require(!campaign.claimed, "claimed");
+
+        campaign.claimed = true;
+        token.transfer(msg.sender, campaign.pledged);
+    }
+
+    function refund(uint256 _id) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp > campaign.endAt, "not ended");
+        require(campaign.pledged < campaign.goal, "pledged < goal");
+
+        uint256 bal = pledgedAmount[_id][msg.sender];
+        pledgedAmount[_id][msg.sender] = 0;
+        token.transfer(msg.sender, bal);
+
+        emit Refund(_id, msg.sender, bal);
+    }
+}
